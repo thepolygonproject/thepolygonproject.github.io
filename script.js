e0e8a5294fc389aa185b95cf1b73a6b31969c98b
thepolygonproject <97771292+thepolygonproject@users.noreply.github.com>
2022-01-23 08:13:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+        var text = "https://" + document.getElementById("input").value;
+        document.getElementById("iframe").src = text;
+        document.getElementById("iframe").height = "100%";
+        document.getElementById("iframe").width = "100%";
+        document.getElementById("content").innerHTML = '';
+    }
+});
+
+function youtube() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Cymuvu%60e%2Ccmm-/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function spotify() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Csrovidy%2Ccmm-uq%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function spotify() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Csrovidy%2Ccmm-uq%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function newgrounds() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Cngwermuldq.aoo%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function newgrounds() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Cngwermuldq.aoo%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function duckduckgo() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-dwcidwcigm.aoo%2F/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function netflix() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/main/hvtrs8%2F-wuw%2Cngtdlkx%2Ccmm-/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function linuxvm() {
+    document.getElementById("iframe").src = "https://copy.sh/v86/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
+
+function simpleunblocker() {
+    document.getElementById("iframe").src = "http://thesimpleunblocker.com/";
+    document.getElementById("iframe").height = "100%";
+    document.getElementById("iframe").width = "100%";
+    document.getElementById("content").innerHTML = '';
+}
